217935fdc4fda0c7720843c6c11622fbdb575cb4
Rafi <smrafi120@gmail.com>
2023-03-11 23:37:20 +0600

    player and enemy movement

--- fightingGame/index.js
+++ fightingGame/index.js
@@ -1,39 +1,178 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024 
+canvas.width = 1024
 canvas.height = 576
 
-c.fillRect(0,0, canvas.width, canvas.height)
-class Sprite {
-    constructor(position, velocity){
-        this.position = position
-        this.velocity = velocity
-    }
+c.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 0.7
 
-    draw(){
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+class Sprite {
+  constructor({ position, velocity, color = 'red' }) {
+    this.position = position
+    this.velocity = velocity
+    this.height = 150;
+    this.lastKey
+    this.attackBox = {
+      position: this.position,
+      with: 100,
+      height: 50
     }
+    this.color = color
+  }
+
+  draw() {
+    c.fillStyle = this.color
+    c.fillRect(this.position.x, this.position.y, 50, this.height)
+
+    //attack box
+    c.fillStyle = 'green'
+    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.with, this.attackBox.height)
+  }
+
+  update() {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      this.velocity.y += gravity
+    }
+  }
 }
 
+
 const player = new Sprite({
+  position: {
     x: 0,
     y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
 })
 
-player.draw()
 
 const enemy = new Sprite({
+  position: {
     x: 400,
-    y: 100
-})
-enemy.draw()
-console.log(player)
+    y: 100,
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  color: 'blue'
 
-function animate(){
-    window.requestAnimationFrame(animate)
-    console.log('go')
+})
+
+// console.log(player)
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  ArrowLeft: {
+    pressed: false
+  }
+}
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+
+  player.velocity.x = 0;
+  enemy.velocity.x = 0;
+
+  //player moving
+  if (keys.a.pressed && player.lastKey === 'a') {
+    player.velocity.x = -5;
+  } else if (keys.d.pressed && player.lastKey === 'd') {
+    player.velocity.x = 5;
+  }
+
+  //enemy moving
+  if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+    enemy.velocity.x = -5;
+  } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+    enemy.velocity.x = 5;
+  }
+
+  //detect for conclision
+  if(player.attackBox.position.x + player.attackBox.with >= enemy.position.x){
+    console.log('Rafi')
+  }
+
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+  console.log(event.key)
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true
+      player.lastKey = 'd'
+      break
+
+    case 'a':
+      keys.a.pressed = true
+      player.lastKey = 'a'
+      break
+
+    case 'w':
+      player.velocity.y = -20
+      break
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = true
+      enemy.lastKey = 'ArrowRight'
+      break
+
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = true
+      enemy.lastKey = 'ArrowLeft'
+      break
+
+    case 'ArrowUp':
+      enemy.velocity.y = -20
+      break
+  }
+  console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+      break
+
+    case 'a':
+      keys.a.pressed = false
+      break
+
+  }
+
+  // enemy keys
+  switch (event.key) {
+    case 'ArrowRight':
+      keys.ArrowRight.pressed = false
+      break
+
+    case 'ArrowLeft':
+      keys.ArrowLeft.pressed = false
+      break
+  }
+  console.log(event.key);
+});
